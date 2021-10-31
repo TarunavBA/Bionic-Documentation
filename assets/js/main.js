fb250570930f9dbbbd68183f4a7062369fea6303
shajin <shajin.sha10@gmail.com>
2021-11-01 03:40:21 +0530

    tepm edited

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,10 +20,8 @@
 
     if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      logo.src = "assets/img/logo/logo-2.svg";
     } else {
       header_navbar.classList.remove("sticky");
-      logo.src = "assets/img/logo/logo.svg";
     }
 
     // show or hide the back-top-top button
